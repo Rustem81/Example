ce45f9f870c99f84e213fd59f89da2a2088e5cd0
ugatu2006 <garantkredo@gmail.com>
2016-12-02 11:49:54 +0500

    тест UTF

--- AddressApp/src/ch/makery/address/MainApp.java
+++ AddressApp/src/ch/makery/address/MainApp.java
@@ -18,7 +18,7 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("AddressApp");
+        this.primaryStage.setTitle("AddressApp_Change");
 
         initRootLayout();
 
